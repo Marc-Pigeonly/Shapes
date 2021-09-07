fe5dd6f6c555429cc4fd9e07271ed76165ff6933
Marc Ford <marc@pigeon.ly>
2021-09-07 11:01:42 -0400

    add some docs, return radius as a float

--- circle.py
+++ circle.py
@@ -1,15 +1,65 @@
-from base_shape import Shape
 import math
 
 
-class Circle(Shape):
+class Circle:
 
     def __init__(self, radius):
         self._radius = radius
         self._diameter = 2 * radius
 
-    def area(self):
-        return math.pi * self._radius * self._radius
+    @property
+    def radius(self):
+        """
+        Radius of a Circle
+        :return: The radius of the circle instance
+        :rtype: float
+        """
+        return float(self._radius)
 
+    @property
+    def area(self):
+        """
+        Area of a Circle
+        :return: The area of the circle instance
+        :rtype: float
+        """
+        return self.area_of_a_circle(self.radius)
+
+    @property
     def perimeter(self):
-        return 2 * math.pi * self._radius
+        """
+        Perimeter of a Circle
+        :return: The perimeter of a circle instance
+        :rtype: float
+        """
+        return self.perimeter_of_a_circle(self.radius)
+
+    @staticmethod
+    def area_of_a_circle(radius):
+        """
+        Function to calculate the area of a circle with a given radius
+        area = PI * R^2
+        :param radius: The radius of the circle
+        :type: float
+        :return: The area of a circle with a given radius
+        :rtype: float
+        """
+        if isinstance(radius, float):
+            return math.pi * (radius * radius)
+        else:
+            raise TypeError("Radius not given as a float")
+
+    @staticmethod
+    def perimeter_of_a_circle(radius):
+        """
+        Function to calculate the perimeter of a circle with a given radius
+        perimeter = 2PI * R
+        :param radius: The radius of the circle
+        :type: float
+        :return: The area of a perimeter with a given radius
+        :rtype: float
+        """
+        if isinstance(radius, float):
+            return 2 * math.pi * radius
+        else:
+            raise TypeError("Radius not given as a float")
